c703ff8a3927339d2d6f13a352adbb52a8313e19
IceReign-MEXT <222150087+IceReign-MEXT@users.noreply.github.com>
2025-09-20 10:23:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,10 @@ load_dotenv()
 # Telegram Bot Token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Admin ID (your Telegram numeric ID)
-ADMIN_ID = os.getenv("ADMIN_ID", "6453658778")  # default to your ID if not set
+# Admin / Owner ID
+OWNER_ID = os.getenv("OWNER_ID")
 
-# CoinMarketCap API
-CMC_API_KEY = os.getenv("CMC_API_KEY")
-
-# Infura / Ethereum
+# Ethereum / Infura
 INFURA_KEY = os.getenv("INFURA_KEY")
 SAFE_ETH_WALLET = os.getenv("SAFE_ETH_WALLET")
 
